10634778c35added606dedbebcdc14e7360f6a93
Mateus Batista <Mateus@MacBook-Pro-de-Adriana.local>
2023-09-30 23:20:12 -0300

    Adicionei programa de estruturar informacoes

---
+++ Formatador-Documento/src/Uf.java
@@ -0,0 +1,14 @@
+public class Uf {
+
+    String nome;
+    String sigla;
+    String capital;
+    String cordenada;
+    int populacaoUltimoSenso;
+    int area;
+    int densidade;
+    String maiorCidade1;
+    String maiorCidade2;
+    String maiorCidade3;
+
+}
